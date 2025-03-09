3cab517bcd9f4342cecf47823b83700992a2de77
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2025-03-09 17:39:27 +0100

    making a basic condional statement

---
+++ math_python.py/CS50P/grade.py
@@ -0,0 +1,12 @@
+score = print(input("Enter your score: "))
+
+if  score >= 90:
+	print("you got an A!")
+elif score >= 80:
+	print("You got an B!")
+elif score >= 70:
+	print("you got a c")
+elif score >= 60:
+	print("ITS A C")
+else:
+	print("failed")
