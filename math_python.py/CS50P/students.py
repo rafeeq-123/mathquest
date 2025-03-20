646df2bd6e3867f5cd1aa38d5d4779377f6ee5c3
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2025-03-20 20:14:02 +0100

    added a dictionary and two methods. one for hardcoded list and one for hardcode dicts

--- math_python.py/CS50P/students.py
+++ math_python.py/CS50P/students.py
@@ -1,4 +1,25 @@
-students = ["student1", "student2", "student3","student4"]
 
-for student in students:
-	print(student)
+def students_list():
+
+	students = ["student1", "student2", "student3","student4"]
+
+	for student in students:
+		print(student)
+
+##students_list()
+## created a method for students
+
+def students_dict():
+
+	students = {
+		"Rafa": "Tims",
+		"Timmy": "Adams",
+		"Donnny": "Stans",
+		"Micheal": "Jordon",
+		"Jerry": "Rice"
+}
+
+	for student in students:
+		print(student, students[student], sep=" + ")
+
+students_dict()		
